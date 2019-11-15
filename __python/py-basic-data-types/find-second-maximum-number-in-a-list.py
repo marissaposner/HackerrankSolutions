4f11e241f6d5968139adb3ef90cb311736187a12
marissaposner <marissaposner14@gmail.com>
2019-11-15 17:07:50 -0500

    updating with new files

---
+++ __python/py-basic-data-types/find-second-maximum-number-in-a-list.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    m=max(arr)
+    while max(arr) == m:
+        arr.remove(max(arr))
+        
+    print(max(arr))
+
+
+
+
+
+
+
